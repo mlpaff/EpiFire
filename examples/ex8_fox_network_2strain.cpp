96ba83eb2d69b6450c32a1eb9fcde07f263abe0d
vagrant <spencer.fox85@gmail.com>
2015-04-30 11:27:07 -0500

    sim and run of 2 strain DE on network 1st commit

---
+++ examples/ex8_fox_network_2strain.cpp
@@ -0,0 +1,50 @@
+#include "Deterministic_Network_TwoStrain_Sim.h"
+
+int main() {
+
+
+    // Setup Initial y[] conditions for initialization
+    double pop_size = 10.0;
+    double theta = 1.0;
+    double pI1 = 1/pop_size;
+    double pI2 = 1/pop_size;
+
+    // Setup model parameters
+    double t0_1 = 0.0;
+    double t0_2 = t0_1;
+    double ps_t0 = 1 - pI1 - pI2;
+    double beta1 = 1.2;
+    double beta2 = 0.2;
+    double gamma1 = 4.0;
+    double gamma2 = 0.25;
+
+    // setup uniform network degree 1-9 as
+    // joel Miller phys Rev paper 2013 Fig 3
+    vector<double> degree_dist;
+    degree_dist.push_back(0.0);
+    degree_dist.push_back(1.0);
+    degree_dist.push_back(1.0);
+    degree_dist.push_back(1.0);
+    degree_dist.push_back(1.0);
+    degree_dist.push_back(1.0);
+    degree_dist.push_back(1.0);
+    degree_dist.push_back(1.0);
+    degree_dist.push_back(1.0);
+    degree_dist.push_back(1.0);
+
+    Deterministic_Network_TwoStrain_Sim sim(t0_1, t0_2, ps_t0, beta1, beta2, gamma1, gamma2, degree_dist);
+    sim.initialize(theta, pI1, pI2);
+    int i = 0;
+    while (i < 100) {
+        sim.printY();
+        sim.step_simulation(1);
+        i++;
+    }
+    sim.printY();
+
+    //sim.run_simulation();
+
+    return 0;
+}
+
+
---
+++ src/Deterministic_Network_TwoStrain_Sim.h
@@ -0,0 +1,113 @@
+#ifndef DETERMINISTIC_NETWORK_TWOSTRAIN_H
+#define DETERMINISTIC_NETWORK_TWOSTRAIN_H
+
+#include <gsl/gsl_odeiv.h>
+#include <gsl/gsl_errno.h>
+#include <iostream>
+#include <math.h>
+#include "DiffEq_Sim.h"
+
+using namespace std;
+
+class Deterministic_Network_TwoStrain_Sim : public DiffEq_Sim {
+
+    private:
+        const double t0_1; //introduction time disease 1
+        const double t0_2; //introduction time disease 2
+        const double ps_t0; //probability neighbor is initially susceptible
+        const vector<double> deg_dist;
+        const double beta1; // transmission rate 1
+        const double beta2; // transmission rate 2
+        const double gamma1; // recovery rate 1
+        const double gamma2; // recovery rate 2
+
+
+    public:
+        Deterministic_Network_TwoStrain_Sim() : t0_1(0.0), t0_2(0.0), ps_t0(0.0),
+            beta1(0.0), beta2(0.0), gamma1(0.0), gamma2(0.0){ nbins=7;}
+        Deterministic_Network_TwoStrain_Sim(double t0_1_param, double t0_2_param, double ps_t0_param, double beta1_param, double beta2_param, double gamma1_param, double gamma2_param, vector<double> deg_dist_param):
+            t0_1(t0_1_param),
+            t0_2(t0_2_param),
+            ps_t0(ps_t0_param),
+            beta1(beta1_param),
+            beta2(beta2_param),
+            gamma1(gamma1_param),
+            gamma2(gamma2_param),
+            deg_dist(deg_dist_param) {
+                nbins=7;
+            }
+        ~Deterministic_Network_TwoStrain_Sim() {};
+
+        void initialize( double theta, double pI1, double pI2) {
+            y = new double[nbins];
+            y[0] = theta;
+            y[1] = pI1;
+            y[2] = pI2;
+            y[3] = pI1;
+            y[4] = pI2;
+            y[5] = 0.0;
+            y[6] = 0.0;
+        }
+
+        double current_susceptible() { return g( y[0] ); }
+        double current_infectious_1() { return y[3]; }
+        double current_infectious_2() { return y[4]; }
+        double current_recovered_1() { return y[5]; }
+        double current_recovered_2() { return y[6]; }
+
+        double current_all_infectious() {
+            return current_infectious_1() + current_infectious_2();
+        }
+
+        double current_all_recovered() {
+            return current_recovered_1() + current_recovered_2();
+        }
+
+
+        double g( double theta) {
+            double val = 0;
+            for (unsigned int i = 0; i<deg_dist.size(); i++) {
+                val += deg_dist[i] * pow(theta,i);
+            }
+            return val;
+        }
+
+        double dg( double theta) {
+            double val = 0;
+            for (unsigned int i = 1; i<deg_dist.size(); i++) {
+                val += (i) * deg_dist[i] * pow(theta,i-1);
+            }
+            return val;
+        }
+
+        double ddg( double theta) {
+            double val = 0;
+            for (unsigned int i = 2; i<deg_dist.size(); i++) {
+                val += (i) * (i-1) *deg_dist[i] * pow(theta,i-2);
+            }
+            return val;
+        }
+
+
+        void derivative(double const y[], double dydt[]) {
+            const double theta = y[0];
+            const double pI1 = y[1];
+            const double pI2 = y[2];
+            const double I1 = y[3];
+            const double I2 = y[4];
+            const double R1 = y[5];
+            const double R2 = y[6];
+
+            dydt[0] = - beta1*pI1 - beta2*pI2;                  // dtheta.dt
+            dydt[1] = -(beta1+gamma1)*pI1 + beta1*pI1*ps_t0*ddg(theta)/dg(1);      // dpI1.dt
+            dydt[2] = -(beta2+gamma2)*pI2 + beta2*pI2*ps_t0*ddg(theta)/dg(1);      // dpI2.dt
+            dydt[3] = beta1*pI1*dg(theta) - gamma1*I1; // dI1.dt
+            dydt[4] = beta2*pI2*dg(theta) - gamma2*I2; // dI2.dt
+            dydt[5] = gamma1*I1;   //dR.dt
+            dydt[6] = gamma2*I2;   //dR.dt
+        }
+
+};
+
+#endif
+
