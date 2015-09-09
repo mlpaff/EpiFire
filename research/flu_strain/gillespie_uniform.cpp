469a29c7608d500cefcf6c9741f287577b60bd8c
vagrant <spencer.fox85@gmail.com>
2015-09-09 00:00:39 -0500

    uniform network runner

---
+++ research/flu_strain/gillespie_uniform.cpp
@@ -0,0 +1,35 @@
+#include "Gillespie_TwoStrain_Network_Sim.h"
+
+int main(int argc,char *argv[]) {
+    if(argc != 5) {
+            printf("Wrong Number of Arguments\n");
+            exit(0);
+    }
+
+    //double alpha1 = argv[1]-0.0;
+    double alpha1, alpha2, beta1, beta2, gamma1, gamma2, phi1, phi2;
+    alpha1 = alpha2 = atof( argv[1] );
+    beta1 = 0.35;
+    beta2 = atof( argv[2] );
+    gamma1 = gamma2 = 1.0/5.0;
+    phi1 = phi2 = atof( argv[4]);
+    int intro_time;
+    intro_time = atoi( argv[3] );
+
+    int num_reps = 1000;
+
+    Network net = Network("gillespie toy", Network::Undirected);
+    net.populate(10000);
+    vector<int> degrees(10000, 5);
+    net.rand_connect_explicit(degrees);
+    for(int i =1; i <= num_reps; i++){
+        Gillespie_TwoStrain_Network_Sim sim(&net, alpha1, alpha2, gamma1, gamma2, beta1, beta2, phi1, phi2, intro_time);
+        cout << "Simulation number: " << i << endl;
+        sim.reset();
+        sim.rand_infect(5, 1);
+        sim.run_simulation(10000.0);
+    }
+    return 0;
+}
+
+
