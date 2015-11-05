b9c1ebdf772ce1da935ad03e90418a7aee8cfa4b
vagrant <spencer.fox85@gmail.com>
2015-11-05 17:08:49 -0600

    param change

--- research/flu_strain/gill_SEIR_exp.cpp
+++ research/flu_strain/gill_SEIR_exp.cpp
@@ -8,7 +8,7 @@ int main(int argc,char *argv[]) {
 
     double alpha1, alpha2, beta1, beta2, gamma1, gamma2, phi1, phi2, eta1, eta2;
     alpha1 = alpha2 = atof( argv[1] );
-    beta1 = 0.07416408;
+    beta1 = 0.07763432;
     beta2 = atof( argv[2] );
     eta1 = eta2 = 1.0/2.62;
     gamma1 = gamma2 = 1.0/3.38;
